57b56ee00623f1762e58be43abc44b99da3742ac
subDesTagesMitExtraKaese <jmueller@justprojects.de>
2020-05-28 23:49:13 +0200

    constraints

--- src/bd/design_1/hdl/design_1_wrapper.vhd
+++ src/bd/design_1/hdl/design_1_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2018.3 (lin64) Build 2405991 Thu Dec  6 23:36:41 MST 2018
---Date        : Thu May 28 22:37:59 2020
+--Date        : Thu May 28 23:21:22 2020
 --Host        : DESKTOP-L9P0FU6 running 64-bit Ubuntu 18.04.4 LTS
 --Command     : generate_target design_1_wrapper.bd
 --Design      : design_1_wrapper
